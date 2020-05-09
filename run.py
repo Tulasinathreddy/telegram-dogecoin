727f1d9fb655a22d279345ebacc9edbb2caa57f3
Tulasinathreddy <34623886+Tulasinathreddy@users.noreply.github.com>
2020-05-09 12:40:23 +0530

    removed block.io payments system

--- run.py
+++ run.py
@@ -1,13 +1,10 @@
 import requests
 import time
-from block_io import BlockIo
 import os
 
 token = os.environ['TELEGRAM_BOT_TOKEN'] #Telegram bot token
 url = "https://api.telegram.org/bot%s/" %(token)
 n = 0
-version = 2
-block_io = BlockIo(os.environ['BLOCKIO_API_KEY'], os.environ['BLOCKIO_PIN'], version)
 active_users = {}
 
 monikers_tuple  = [
@@ -32,96 +29,78 @@ def getCount(chatid):
 def sendMsg(message,chatid):
 	requests.get(url + "sendMessage", data={"chat_id":chatid,"text":message})
 
-def returnBal(username):
-	data = block_io.get_address_balance(labels=username)
-	balance = data['data']['balances'][0]['available_balance']
-	pending_balance = data['data']['balances'][0]['pending_received_balance']
-	return (balance, pending_balance)
-
 def process(message,username,chatid):
 	message = message.split(" ")
 	for i in range(message.count(' ')):
 		message.remove(' ')
 		
 	if "/register" in message[0]:
-		try:
-			block_io.get_new_address(label=username)
+		#try:
+			#block_io.get_new_address(label=username)
 			sendMsg("@"+username+" you are now registered.",chatid)
-		except:
+		#except:
 			sendMsg("@"+username+" you are already registered.",chatid)
 	elif "/start" in message:
 		    sendMsg("Welcoe the tipper bot",chatid)
 	elif "/balance" in message[0]:
-		try:
-			(balance, pending_balance) = returnBal(username)
-			sendMsg("@"+username+" Balance : "+balance+ "Doge ("+pending_balance+" Doge)",chatid)
-		except:
+		#try:
+		#	(balance, pending_balance) = returnBal(username)
+			sendMsg("@"+username+" Balance : Doge",chatid)
+		#except:
 			sendMsg("@"+username+" you are not registered yet. use /register to register.",chatid)
 	elif "/tip" in message[0]:
-		try:
-			person = message[1].replace('@','')
-			amount_msg = 1 if message[2] in ('a', 'an', '1') else message[2]
-			amount = abs(float(amount_msg)) * monikers_dict.get(message[3], 1)
+		#try:
+			#person = message[1].replace('@','')
+			#amount_msg = 1 if message[2] in ('a', 'an', '1') else message[2]
+			#amount = abs(float(amount_msg)) * monikers_dict.get(message[3], 1)
 
-			if monikers_dict.get(message[3], 0) == 0:
-				sin_plu = "doge"
-			elif amount_msg == 1:
-				sin_plu = monikers_tuple[monikers_flat.index(message[3])//3][0]
-			else:
-				sin_plu = monikers_tuple[monikers_flat.index(message[3])//3][1]
+			#if monikers_dict.get(message[3], 0) == 0:
+			#	sin_plu = "doge"
+			#elif amount_msg == 1:
+			#	sin_plu = monikers_tuple[monikers_flat.index(message[3])//3][0]
+			#else:
+			#	sin_plu = monikers_tuple[monikers_flat.index(message[3])//3][1]
 
-			block_io.withdraw_from_labels(amounts=str(amount), from_labels=username, to_labels=person)
-			sendMsg("@"+username+" tipped "+ str(amount_msg) + " " + sin_plu +
-					("" if monikers_dict.get(message[3], 0) == 0 else f" ({str(amount)} doge)") +
-					" to @"+person+"",chatid)
-			(balance, pending_balance) = returnBal(person)
-			sendMsg("@"+person+" Balance : "+balance+ "Doge ("+pending_balance+" Doge)",chatid)
-		except ValueError:
+			#block_io.withdraw_from_labels(amounts=str(amount), from_labels=username, to_labels=person)
+			#sendMsg("@"+username+" tipped "+ str(amount_msg) + " " + sin_plu +
+					#("" if monikers_dict.get(message[3], 0) == 0 else f" ({str(amount)} doge)") +
+					#" to @"+person+"",chatid)
+			#(balance, pending_balance) = returnBal(person)
+			#sendMsg("@"+person+" Balance : "+balance+ "Doge ("+pending_balance+" Doge)",chatid)
+		#except ValueError:
 			sendMsg("@"+username+" invalid amount.",chatid)
-		except:
-			sendMsg("@"+username+" insufficient balance or @"+person+" is not registered yet.",chatid)
+		#except:
+			sendMsg("@"+username+" insufficient balance or @ is not registered yet.",chatid)
 	elif "/address" in message[0]:
-		try:
-			data = block_io.get_address_by_label(label=username)
-			sendMsg("@"+username+" your address is "+data['data']['address']+"",chatid)
-		except:
+		#try:
+			#data = block_io.get_address_by_label(label=username)
+			#sendMsg("@"+username+" your address is "+data['data']['address']+"",chatid)
+		#except:
 			sendMsg("@"+username+" you are not registered yet. use /register to register.",chatid)
 	elif "/withdraw" in message[0]:
-		try:
+		#try:
 			amount = abs(float(message[1]))
 			address = message[2]
-			data = block_io.withdraw_from_labels(amounts=str(amount), from_labels=username, to_addresses=address)
-		except ValueError:
+			#data = block_io.withdraw_from_labels(amounts=str(amount), from_labels=username, to_addresses=address)
+		#except ValueError:
 			sendMsg("@"+username+" invalid amount.",chatid)
-		except:
+		#except:
 			sendMsg("@"+username+" insufficient balance or you are not registered yet.",chatid)
 
 	elif "/rain" in message[0]:
-		try:
-			users = getCount(chatid)
-			if username in users:
-				users.remove(username)
-			number = len(users)
-
-			amount = ("10," * (number - 1)) + '10'
-			name = username
-			username = ((username+',') * (number - 1)) + username
-			if number < 2:
-				sendMsg("@"+username+" less than 2 shibes are active.",chatid)
-			else:
-				print(amount)
-				print(username)
-				block_io.withdraw_from_labels(amounts=amount, from_labels=username, to_labels=','.join(users))
-				sendMsg("@"+name+" is raining on "+','.join(users)+"",chatid)
-		except:
-			pass
-
+	     sendMsg("UNDER MAINTAINCE",chatid)
+	    
 	elif "/monikers" in message:
-		sendMsg("--MONIKERS--\n" +
+		 sendMsg("--MONIKERS--\n" +
 			monikers_str,chatid)
 
 	elif "/active" in message:
-		sendMsg("Current active : %d shibes" %(len(getCount(chatid))),chatid)
+		 try:
+			 
+			sendMsg("Current active : %d shibes" %(len(getCount(chatid))),chatid)
+	     except
+		     sendMsg("UNDER MAINTAINCE",chatid)
+	     pass 
 	else:
 		global active_users
 		try:
